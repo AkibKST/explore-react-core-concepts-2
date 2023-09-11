92836b96da891412a2b7ea912a4178f9fef3cb00
AkibKST <akibkst22@gmail.com>
2023-09-11 12:58:21 +0600

    useState, useEffect

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css';
+import Counter from './counter';
+import Friends from './Friends';
+import Team from './Team';
+import Users from './Users';
+
+function App() {
+
+  function handleClick(){
+    alert('button clicked');
+  }
+
+const handleClick2 = () =>{
+  alert('button 2 clicked');
+}
+
+const addToFive = (num) =>{
+  alert(num + 5)
+}
+ 
+  return (
+    <>
+      <h3>React Core Concepts 2</h3>
+
+      <Friends></Friends>
+
+      <Users></Users>
+
+      <Team></Team>
+
+      <Counter></Counter>
+
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={()=>{alert('3rd clicked')}}>Click 3</button>
+      <button onClick={()=> addToFive(3)}>Click four</button>
+    </>
+  )
+}
+
+export default App
